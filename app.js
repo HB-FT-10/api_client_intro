eab9cba7975fc453a24b559ea9c0bd9ba03565fd
ld-web <lucas@ld-web.net>
2025-04-14 12:52:49 +0200

    :sparkles: add User form

--- app.js
+++ app.js
@@ -1,16 +1,42 @@
 const usersContainer = document.querySelector("#users");
+const userForm = document.querySelector("#newUserForm");
 
 window.onload = async () => {
   try {
-    const res = await fetch("http://localhost:8005");
+    const res = await fetch("http://localhost:8005/users");
     const users = await res.json();
 
     users.forEach((user) => {
       const userDiv = document.createElement("div");
-      userDiv.innerText = `${user.name} ${user.firstname}`;
+      userDiv.innerText = `${user.username} / ${user.email}`;
       usersContainer.appendChild(userDiv);
     });
   } catch (err) {
     console.error(err);
   }
 };
+
+userForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const data = new FormData(userForm);
+  const username = data.get("username");
+  const email = data.get("email");
+
+  const res = await fetch("http://localhost:8005/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username,
+      email,
+    }),
+  });
+
+  if (res.status === 201) {
+    console.log("Success !");
+    const userDiv = document.createElement("div");
+    userDiv.innerText = `${username} / ${email}`;
+    usersContainer.appendChild(userDiv);
+  }
+});
